9c9663f3a9cda4242a432b4a0a02cead9778511c
Donald Geddes <hi@donaldgeddes.ca>
2022-01-24 11:36:50 -0800

    Improve build pipeline

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import { terser } from 'rollup-plugin-terser'
+import analyze from 'rollup-plugin-analyzer'
 import pkg from './package.json'
 
 const input = 'src/index.js'
@@ -5,11 +7,17 @@ const input = 'src/index.js'
 const core = ['node:fs', 'node:path']
 const external = [...core, ...Object.keys(pkg.dependencies)]
 
-export default {
-  input,
-  external,
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'esm' },
-  ],
-}
+export default [
+  {
+    input,
+    external,
+    output: { file: pkg.main, format: 'cjs' },
+    plugins: [terser({ keep_classnames: true, keep_fnames: true }), analyze()],
+  },
+  {
+    input,
+    external,
+    output: { file: pkg.module, format: 'esm' },
+    plugins: [terser({ keep_classnames: true, keep_fnames: true }), analyze()],
+  },
+]
